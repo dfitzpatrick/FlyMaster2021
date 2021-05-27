a06084f430a66e22eede9de3b6b84939cdeab1a6
Derek Fitzpatrick <dfitz.murrieta@gmail.com>
2021-05-26 17:46:37 -0700

    Directory structure

---
+++ FlyMaster2021/business/FlyImporter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlyMaster2021.business
+{
+    class FlyImporter
+    {
+        /// <summary>
+        /// Main constructor for the importer. 
+        /// </summary>
+        /// <param name="flyDirectory">The directory to read .CSV files</param>
+        public FlyImporter(string flyDirectory)
+        {
+            return;
+        }
+    }
+}
---
+++ FlyMaster2021/data/FlyDAL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlyMaster2021.data
+{
+    /// <summary>
+    /// The main data access layer for the Fly Master Project
+    /// </summary>
+    class FlyDAL
+    {
+    }
+}
